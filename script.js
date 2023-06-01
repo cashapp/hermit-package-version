3bdcc505c09dd3b323a5fcf49bf03d48568f7352
Scott Robinson <ssr@squareup.com>
2023-06-01 14:38:08 +1000

    workflows: test

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 module.exports = async ({ package, cwd, core, exec }) => {
   await exec
-    .exec("hermit", ["info", package, "--json"], {
+    .exec("hermit", ["list", "--json"], {
       cwd,
       listeners: {
         stdout: (data) => {
           const info = JSON.parse(data.toString());
-          core.setOutput("version", info.version);
+          const layer = info.find(l => l.Reference.Name == package)
+          const version = layer?.Reference.Version;
+
+          if (!package) {
+            core.setFailed(`${package} not found`);
+          } else if (!version) {
+            core.setFailed(`No version found for ${package}, version: ${version}`);
+          } else {
+            core.setOutput("version", version);
+          }
         },
       },
     })
-    .catch((error) => {
-      core.setFailed(error.message);
-    });
+    .catch((error) => core.setFailed(error.message));
 };
